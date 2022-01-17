1b90f843befd4685458b6c5110f53f5295b85dd4
zl <themorecolor@gmail.com>
2022-01-17 08:15:52 +0800

    pub publish

--- lib/three3d/renderers/webgl/index.dart
+++ lib/three3d/renderers/webgl/index.dart
@@ -1,29 +1,14 @@
 library three_webgl;
 
-import 'dart:collection';
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:developer' as developer;
 
 import 'package:flutter/foundation.dart';
+import 'package:three_dart/three3d/utils.dart';
 import 'package:three_dart/three_dart.dart';
 import 'package:flutter_gl/flutter_gl.dart';
 
-import 'package:three_dart/three3d/WeakMap.dart';
-import 'package:three_dart/three3d/cameras/index.dart';
-import 'package:three_dart/three3d/constants.dart';
-import 'package:three_dart/three3d/core/index.dart';
-import 'package:three_dart/three3d/dartHelpers.dart';
-import 'package:three_dart/three3d/extras/index.dart';
-import 'package:three_dart/three3d/geometries/index.dart';
-import 'package:three_dart/three3d/lights/index.dart';
-import 'package:three_dart/three3d/materials/index.dart';
-import 'package:three_dart/three3d/math/index.dart';
-import 'package:three_dart/three3d/objects/index.dart';
-import 'package:three_dart/three3d/renderers/index.dart';
-import 'package:three_dart/three3d/renderers/shaders/index.dart';
-import 'package:three_dart/three3d/textures/index.dart';
-import 'package:three_dart/three3d/utils.dart';
 
 part './WebGLAnimation.dart';
 part './WebGLAttributes.dart';
